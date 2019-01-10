c1c43c1cfe7d70cc87cda9d960388696eda64646
Bagus Nusantara <bagus.nusantara313@gmail.com>
2019-01-10 14:39:20 +0700

    membuka lembaran baru

--- resources/views/layouts/js.blade.php
+++
@@ -1,4 +0,0 @@
-<script src="{{ asset('assets/datatables/jquery.dataTables.js') }}"></script>
-<script src="{{ asset('assets/datatables/dataTables.bootstrap4.js') }}"></script>
-<!-- Demo scripts for this page-->
-<script src="{{ asset('js/demo/datatables-demo.js') }}"></script>
